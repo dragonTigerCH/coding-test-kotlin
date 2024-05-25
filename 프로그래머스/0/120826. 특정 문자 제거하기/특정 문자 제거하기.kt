b53d8e932556ee165eb96af87fd9f9af8f6c2e55
dragonTigerCH <97586086+dragonTigerCH@users.noreply.github.com>
2024-05-26 02:14:36 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(my_string: String, letter: String): String {
-        val charArray = letter.toCharArray()
-        val answer = my_string.filter { (it != charArray[0]) }
-        return answer
+        return my_string.filterNot { it == letter[0]}
     }
 }
