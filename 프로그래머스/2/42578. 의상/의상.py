fb40f4a0e0015c188ab4da5d4ce614f98e982f99
dragonTigerCH <97586086+dragonTigerCH@users.noreply.github.com>
2024-07-16 23:43:36 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,17 +1,13 @@
-from collections import defaultdict
 def solution(clothes):
-    
+    from collections import defaultdict
     # 의상의 종류별로 개수를 저장할 딕셔너리
     clothes_dict = defaultdict(int)
-    
     # 각 의상을 종류별로 카운트
     for item, category in clothes:
         clothes_dict[category] += 1
-    
     # 경우의 수 계산
     combinations = 1
     for count in clothes_dict.values():
         combinations *= (count + 1)
-    
     # 아무 것도 입지 않는 경우 1가지를 제외
     return combinations - 1
